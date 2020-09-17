cbe963ef221a26856c92034d927be947cc206fc1
Ignotus111 <romainlevasseur4@gmail.com>
2020-09-17 18:19:38 +0200

    correction

--- api_request.py
+++ api_request.py
@@ -4,24 +4,21 @@ import json
 from constants import *
 from testclass import *
 
-def requestprod():
+def requestprod(link):
     i = 1
-    link = "https://fr.openfoodfacts.org/categorie/aliments-et-boissons-a-base-de-vegetaux"
     urls = []
     while i < NB_PAGEPRODUCT + 1:
         urls.append(link+"/"+str(i))
         i += 1
-
     print(urls)
     for l in urls :
         response = requests.request("GET", l+".json")
         json_prod = response.json()
         products_prod = json_prod.get('products')
         prod_code = [(data.get('id'), data.get('product_name'), data.get('generic_name'), data.get('url'), data.get('stores'), data.get('nutriscore_grade')) for data in products_prod]
-        for prod in prod_code:
-            product = Product(prod[0],prod[1],prod[2],prod[3],prod[4],prod[5])
-            product.save()
-            print(prod[1], " a été enregistré avec succés!")
+        prod_code = [Product]
+
+        Product().saveMany(prod_code)
 
 def requestcat():
 
@@ -32,11 +29,11 @@ def requestcat():
     link_cat = [(data.get('url'),) for data in tags_cat]
     for link in link_cat :
         print(link[0])
-        #requestprod(link[0])
+        requestprod(link[0])
     for cat in name_cat:
         category = Categories(cat[0])
         category.saveCat()
         print(cat," a été enregistré avec succés!")
 
-requestprod()
+requestcat()
 ##    url = f'"{link}/1.json"'
